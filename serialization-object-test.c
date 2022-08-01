25ec1fa655f576ceaddc3069a382e220ad0992db
Erlend <erlend_hapnes@hotmail.com>
2022-08-01 18:41:47 +0200

    Added serialization test

---
+++ serialization-object-test.c
@@ -0,0 +1,173 @@
+#include "serialization-object-test.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define COMBINED_TEST 1
+
+static const char *ADDRESS_FORMAT_OUT = "%d:%d";
+static const char *HOP_FORMAT_IN = " %d, %d, %d:%d";
+//static const char *HOP_FORMAT_IN = "\n%d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, \
+//%d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, \
+//%d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, \
+//%d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d, %d, %d, %d:%d";
+static const char *HOP_FORMAT_OUT = "%d, %d, %d:%d ";
+// static const char *TR_TEST_FORMAT_OUT = "%d, %s, %x%x:%x%x:%x%x:%x%x, %d, %x%x:%x%x:%x%x:%x%x, %d, %[^,], %d, ";
+static const char *TR_TEST_FORMAT_IN = "\n%d, %[^,], %d:%d, %d, %d:%d, %d, %[^,]";
+static const char *TR_TEST_FORMAT_OUT = "%d, %s, %d:%d, %d, %d:%d, %d, %s,\n";
+
+int main(void)
+{
+    hop hop_arr1[35];
+    hop hop_arr2[35];
+
+    address a1_2 = {
+        .high_order_bits = 77,
+        .low_order_bits = 88,
+    };
+    address a1_3 = {
+        .high_order_bits = 99,
+        .low_order_bits = 11,
+    };
+    address a1 = {
+        .high_order_bits = 55,
+        .low_order_bits = 44,
+    };
+
+    hop h1_2 = {
+        .returned_flowlabel = 34,
+        .hopnumber = 52,
+        .hop_address = a1,
+    };
+
+    hop h1 = {
+        .returned_flowlabel = 34,
+        .hopnumber = 52,
+        .hop_address = a1,
+    };
+
+    traceroute t1 = {
+        .outgoing_tcp_port = 443,
+        .timestamp = "hello t1",
+        .source_ip = a1_2,
+        .source_asn = 4,
+        .destination_ip = a1_3,
+        .destination_asn = 59,
+        .path_id = "path_id",
+    };
+
+    hop_arr1[0] = h1;
+    hop_arr1[1] = h1_2;
+
+    for (int i = 2; i < 35; i++)
+    {
+        address *a;
+        a = malloc(sizeof(address));
+        a->high_order_bits = 55;
+        a->low_order_bits = 44;
+        hop *h;
+        h = malloc(sizeof(hop));
+        h->returned_flowlabel = 30;
+        h->hopnumber = 6;
+        h->hop_address = *a;
+        hop_arr1[i] = *h;
+    }
+    traceroute *t2 = malloc(sizeof(traceroute));
+
+    FILE *file;
+    if ((file = fopen("waffles.dat", "w+")) == 0)
+    {
+        return 1;
+    }
+
+#ifdef TR_TEST
+
+    /* Write to file */
+    fprintf(file, TR_TEST_FORMAT_OUT, t1.outgoing_tcp_port, t1.timestamp, t1.source_ip.high_order_bits, t1.source_ip.low_order_bits,
+            t1.source_asn, t1.destination_ip.high_order_bits, t1.destination_ip.low_order_bits, t1.destination_asn,
+            t1.path_id);
+
+    /* Read from file */
+    rewind(file);
+    fscanf(file, TR_TEST_FORMAT_IN, &t2->outgoing_tcp_port, t2->timestamp, &t2->source_ip.high_order_bits, &t2->source_ip.low_order_bits,
+           &t2->source_asn, &t2->destination_ip.high_order_bits, &t2->destination_ip.low_order_bits, &t2->destination_asn,
+           t2->path_id);
+
+    printf(TR_TEST_FORMAT_OUT, t2->outgoing_tcp_port, t2->timestamp, t2->source_ip.high_order_bits, t2->source_ip.low_order_bits,
+           t2->source_asn, t2->destination_ip.high_order_bits, t2->destination_ip.low_order_bits, t2->destination_asn,
+           t2->path_id);
+
+    fclose(file);
+
+#endif
+
+#ifdef HOP_TEST
+
+    if ((file = fopen("hops.dat", "w+")) == 0)
+    {
+        return 1;
+    }
+
+    /* Write to file */
+    for (int i = 0; i < 35; i++)
+    {
+        fprintf(file, HOP_FORMAT_OUT, hop_arr1[i].returned_flowlabel, hop_arr1[i].hopnumber, hop_arr1->hop_address.high_order_bits, hop_arr1->hop_address.low_order_bits);
+    }
+    // fprintf(file, "\n");
+    puts("Done writing to file!");
+
+    /* Read from file */
+    rewind(file);
+    // fscanf(file, HOP_FORMAT_IN, &hop_arr2[0].returned_flowlabel, &hop_arr2[0].hopnumber, &hop_arr2[0].hop_address.high_order_bits, &hop_arr2[0].hop_address.low_order_bits);
+    // printf(HOP_FORMAT_OUT, hop_arr2[0].returned_flowlabel, hop_arr2[0].hopnumber, hop_arr2[0].hop_address.high_order_bits, hop_arr2[0].hop_address.low_order_bits);
+    for (int i = 0; i < 35; i++)
+    {
+        fscanf(file, HOP_FORMAT_IN, &hop_arr2[i].returned_flowlabel, &hop_arr2[i].hopnumber, &hop_arr2[i].hop_address.high_order_bits, &hop_arr2[i].hop_address.low_order_bits);
+    }
+    puts("Done reading from file!");
+
+    for (int i = 0; i < 35; i++)
+    {
+        printf(HOP_FORMAT_OUT, hop_arr2[i].returned_flowlabel, hop_arr2[i].hopnumber, hop_arr2[i].hop_address.high_order_bits, hop_arr2[i].hop_address.low_order_bits);
+    }
+#endif
+
+#ifdef COMBINED_TEST
+
+    /* Write to file */
+    fprintf(file, TR_TEST_FORMAT_OUT, t1.outgoing_tcp_port, t1.timestamp, t1.source_ip.high_order_bits, t1.source_ip.low_order_bits,
+            t1.source_asn, t1.destination_ip.high_order_bits, t1.destination_ip.low_order_bits, t1.destination_asn,
+            t1.path_id);
+    for (int i = 0; i < 35; i++)
+    {
+        fprintf(file, HOP_FORMAT_OUT, t1.hops[i].returned_flowlabel, t1.hops[i].hopnumber, t1.hops[i].hop_address.high_order_bits, t1.hops[i].hop_address.low_order_bits);
+    }
+
+    /* Read from file */
+    rewind(file);
+    fscanf(file, TR_TEST_FORMAT_IN, &t2->outgoing_tcp_port, t2->timestamp, &t2->source_ip.high_order_bits, &t2->source_ip.low_order_bits,
+           &t2->source_asn, &t2->destination_ip.high_order_bits, &t2->destination_ip.low_order_bits, &t2->destination_asn,
+           t2->path_id);
+    for (int i = 0; i < 35; i++)
+    {
+        fscanf(file, HOP_FORMAT_IN, &t1.hops[i].returned_flowlabel, &t1.hops[i].hopnumber, &t1.hops[i].hop_address.high_order_bits, &t1.hops[i].hop_address.low_order_bits);
+    }
+    puts("Done reading from file!");
+
+    printf(TR_TEST_FORMAT_OUT, t2->outgoing_tcp_port, t2->timestamp, t2->source_ip.high_order_bits, t2->source_ip.low_order_bits,
+           t2->source_asn, t2->destination_ip.high_order_bits, t2->destination_ip.low_order_bits, t2->destination_asn,
+           t2->path_id);
+    for (int i = 0; i < 35; i++)
+    {
+        printf(HOP_FORMAT_OUT, t1.hops[i].returned_flowlabel, t1.hops[i].hopnumber, t1.hops[i].hop_address.high_order_bits, t1.hops[i].hop_address.low_order_bits);
+    }
+
+    fclose(file);
+#endif
+    return 0;
+}
+
+// int serialize()
+// {
+
+// }
---
+++ serialization-object-test.h
@@ -0,0 +1,39 @@
+#include <stdint.h>
+
+typedef struct ipv6_address
+{
+    long high_order_bits;
+    long low_order_bits;
+} address;
+
+typedef struct hop
+{
+    uint32_t returned_flowlabel;
+    uint8_t hopnumber;
+    address hop_address; // Could be a list of address pointers
+} hop;
+
+typedef struct traceroute
+{
+    /*
+        "outgoing_tcp_port": "443",
+        "flow_label": "1048575",
+        "timestamp": "2022-05-07 15:50:47.559550",
+        "source": "2a03:b0c0:1:d0::b45:6001",
+        "source_asn": "14061",
+        "destination": "2600:9000:20a5:f800:10:15f0:8cc0:93a1",
+        "destination_asn": "16509",
+        "path_id": "c0f8ed8a7c1f3d725bd89de7ed7eced0b9dcc67b",
+    */
+    uint16_t outgoing_tcp_port;
+    char *timestamp;
+    address source_ip;
+    uint32_t source_asn;
+    address destination_ip;
+    uint32_t destination_asn;
+    char *path_id;
+    uint32_t hop_count;
+
+    hop hops[35]; // maximum hop length is 35. any hops longer than that do not get included.
+
+} traceroute;
